c3e3839fd9f0d591b23124a46922004c9d7a1bcf
lamech <27463+lamech@users.noreply.github.com>
2021-05-08 00:34:38 +0000

    Begin migration to sqlite3.

--- aggrejag.js
+++ aggrejag.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const jsonfile = require('jsonfile')
-const outfile = 'links.jsonl'
+const sqlite3 = require('sqlite3').verbose();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -20,9 +20,12 @@ client.on("message", function(message) {
     if (command === "ping") {
       const timeTaken = Date.now() - message.createdTimestamp;
       message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    } else if (command === "help") {
+      message.reply(`I grab links every time someone shares them in here. Pretty soon I'll be able to present them back to you in various ways. Stay tuned!`);
     }
   } else {
 
+    const outfile = 'links.jsonl'; 
     const words = message.content.split(' ');
 
     let count = 0;
@@ -30,8 +33,8 @@ client.on("message", function(message) {
       if (isValidHttpUrl(s)) {
         let stream = fs.createWriteStream("links.jsonl", {flags:'a'});
         let date = new Date().toISOString();
-        console.log(date + " Saving this link from " + message.author.tag + ": " + s);
-        let entry = { url: s, date: date };
+        console.log(date + " Saving this link from " + message.guild.name + ' ' + message.channel.name + ' ' + message.author.tag + ": " + s);
+        let entry = { url: s, date: date, server: message.guild.name, channel: message.channel.name };
   
         jsonfile.writeFile(outfile, entry, { flag: 'a' }, function (err) {
           if (err) console.error(err)
@@ -47,6 +50,10 @@ client.on("message", function(message) {
       message.reply('Saved ' + count + links);
     }
   }
+});
+
+client.once('ready', () => {
+	console.log('Aggrejag ready.');
 });
 
 client.login(auth.token);
